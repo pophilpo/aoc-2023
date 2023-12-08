f080280231004ab56f7099579cf9d18dd30e672f
Popov Philipp <philolippo@gmail.com>
2023-12-08 18:41:51 +0100

    Implement reading the input

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,24 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    solve().unwrap();
+}
+
+fn solve() -> Result<i32, Box<dyn std::error::Error>> {
+    if let Ok(lines) = read_lines("./input.txt") {
+        for line in lines {
+            println!("{}", line.unwrap());
+        }
+    }
+    Ok(12)
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
