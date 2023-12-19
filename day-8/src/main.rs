c75f8b526027d8fa71b2f44954b2ef61bd015b13
Popov Philipp <philolippo@gmail.com>
2023-12-19 21:42:42 +0100

    Complete Day-8 part two

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
+#[derive(Debug)]
 struct Node {
     value: String,
     left: usize,
@@ -104,9 +105,59 @@ fn solve_part_one(filename: &str) -> Result<u32, Box<dyn std::error::Error>> {
     Ok(answer)
 }
 
+fn solve_part_two(filename: &str) -> Result<u64, Box<dyn std::error::Error>> {
+    // This answer is specific to the exact problem input
+    let (instructions, nodes) = parse_input(filename)?;
+
+    let starting_nodes = nodes.iter().filter(|&node| node.value.ends_with("A"));
+
+    for node in starting_nodes.clone() {
+        println!("{:?}", node);
+    }
+
+    let mut current_instruction_index = 0;
+
+    let mut answer: u64 = 1;
+    for mut current_node in starting_nodes {
+        let mut local_answer = 0;
+
+        while !current_node.value.ends_with("Z") {
+            if current_instruction_index >= instructions.len() {
+                current_instruction_index = 0;
+            }
+
+            let direction = instructions.chars().nth(current_instruction_index).unwrap();
+            match direction {
+                'L' => current_node = &nodes[current_node.left],
+                'R' => current_node = &nodes[current_node.right],
+                _ => unreachable!(),
+            }
+
+            current_instruction_index += 1;
+            local_answer += 1;
+        }
+
+        // Magic numbers :D
+        // Actually 277 is the only common denominator of answer per path found
+        // The Part two task boils down to finding the minimum number that is divisible by all paths lenghts
+        // So before I got the number 277, I printed solve_part_one() for each Node that ends with an A and found 277 from there
+        let unique_multiplier = local_answer / 277;
+
+        println!(
+            "First cycle is {} steps which is 277*{} ",
+            local_answer, unique_multiplier
+        );
+        answer *= unique_multiplier;
+    }
+
+    // Don't forget the last prime
+    answer *= 277;
+    Ok(answer)
+}
+
 fn main() {
     let filename = "input.txt";
-    let answer = solve_part_one(filename).unwrap();
+    let answer = solve_part_two(filename).unwrap();
     println!("{}", answer);
 }
 
